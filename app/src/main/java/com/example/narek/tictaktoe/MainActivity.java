3c30535a296c79cc47eb8f48f859a28655e44a87
Aram Safaryan <aram03007@mail.ru>
2016-03-24 13:40:38 +0400

    COMP LOGIC

--- app/src/main/java/com/example/narek/tictaktoe/MainActivity.java
+++ app/src/main/java/com/example/narek/tictaktoe/MainActivity.java
@@ -6,13 +6,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.RadioButton;
 
 public class MainActivity extends AppCompatActivity {
     private int[][] board = {
-            {0,0,0},
-            {0,0,0},
-            {0,0,0}
+            {0, 0, 0},
+            {0, 0, 0},
+            {0, 0, 0}
     };
+    private int[][] preferredMoves = {
+            {1, 1}, {0, 0}, {0, 2}, {2, 0}, {2, 2},
+            {0, 1}, {1, 0}, {1, 2}, {2, 1}};
     private boolean isFirstPersonTurn = true;
     ChekWinner chekWinner;
 
@@ -35,93 +39,176 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
-        crossView.updateBoard(board);
+        final RadioButton twoplayers = (RadioButton) findViewById(R.id.radioButton2);
+//        crossView.updateBoard(board);
         crossView.setOnSellTapListener(new CrossView.OnSellTapListener() {
             @Override
             public void onSellTapped(int row, int col) {
-                if (isFirstPersonTurn) {
+                assert twoplayers != null;
+                if (twoplayers.isChecked()) {
+                    if (isFirstPersonTurn) {
                         board[row][col] = 1;
 
 
+                    } else {
+                        board[row][col] = -1;
 
-                } else  {
-                    board[row][col] = -1;
+                    }
+
+                    isFirstPersonTurn = !isFirstPersonTurn;
+                    crossView.updateBoard(board);
+
+                    chekWinner = gameOver(board);
+
+                    if (chekWinner == ChekWinner.X_WIN) {
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Game Over")
+                                .setMessage("X Player Win")
+                                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+
+                                    }
+                                })
+                                .setPositiveButton("NEW GAME", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        resetBoard();
+                                        crossView.updateBoard(board);
+
+
+                                    }
+                                })
+
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                    } else if (chekWinner == ChekWinner.O_WIN) {
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Game Over")
+                                .setMessage("O Player Win")
+                                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+
+                                    }
+                                })
+                                .setPositiveButton("NEW GAME", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        resetBoard();
+                                        crossView.updateBoard(board);
+
+
+                                    }
+                                })
+
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                    } else if (chekWinner == ChekWinner.DRAW) {
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Game Over")
+                                .setMessage("Draw")
+                                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+
+                                    }
+                                })
+                                .setPositiveButton("NEW GAME", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        resetBoard();
+                                        crossView.updateBoard(board);
+
+
+                                    }
+                                })
+
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                    }
+                } else {
+                    board[row][col] = 1;
+                    crossView.updateBoard(board);
+                    chekWinner = gameOver(board);
+
+
+                    int[] tmp = move();
+                    if (tmp != null) {
+                        board[tmp[0]][tmp[1]] = -1;
+                        crossView.updateBoard(board);
+
+                    }
+
+                    if (chekWinner == ChekWinner.X_WIN) {
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Game Over")
+                                .setMessage("X Player Win")
+                                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+
+                                    }
+                                })
+                                .setPositiveButton("NEW GAME", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        resetBoard();
+                                        crossView.updateBoard(board);
+
+
+                                    }
+                                })
+
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                    } else if (chekWinner == ChekWinner.O_WIN) {
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Game Over")
+                                .setMessage("O Player Win")
+                                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+
+                                    }
+                                })
+                                .setPositiveButton("NEW GAME", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        resetBoard();
+                                        crossView.updateBoard(board);
+
+
+                                    }
+                                })
+
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                    } else if (chekWinner == ChekWinner.DRAW) {
+                        new AlertDialog.Builder(MainActivity.this)
+                                .setTitle("Game Over")
+                                .setMessage("Draw")
+                                .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
+                                    @Override
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        finish();
+
+                                    }
+                                })
+                                .setPositiveButton("NEW GAME", new DialogInterface.OnClickListener() {
+                                    public void onClick(DialogInterface dialog, int which) {
+                                        resetBoard();
+                                        crossView.updateBoard(board);
+
+
+                                    }
+                                })
+
+                                .setIcon(android.R.drawable.ic_dialog_alert)
+                                .show();
+                    }
 
                 }
-
-                isFirstPersonTurn = !isFirstPersonTurn;
-                crossView.updateBoard(board);
-
-                chekWinner = gameOver(board);
-
-                if (chekWinner == ChekWinner.X_WIN) {
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("Game Over")
-                            .setMessage("X Player Win")
-                            .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    finish();
-
-                                }
-                            })
-                            .setPositiveButton("NEW GAME", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    resetBoard();
-                                    crossView.updateBoard(board);
-
-
-                                }
-                            })
-
-                            .setIcon(android.R.drawable.ic_dialog_alert)
-                            .show();
-                }else if (chekWinner == ChekWinner.O_WIN) {
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("Game Over")
-                            .setMessage("O Player Win")
-                            .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    finish();
-
-                                }
-                            })
-                            .setPositiveButton("NEW GAME", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    resetBoard();
-                                    crossView.updateBoard(board);
-
-
-                                }
-                            })
-
-                            .setIcon(android.R.drawable.ic_dialog_alert)
-                            .show();
-                }else if (chekWinner == ChekWinner.DRAW) {
-                    new AlertDialog.Builder(MainActivity.this)
-                            .setTitle("Game Over")
-                            .setMessage("Draw")
-                            .setNegativeButton("Exit", new DialogInterface.OnClickListener() {
-                                @Override
-                                public void onClick(DialogInterface dialog, int which) {
-                                    finish();
-
-                                }
-                            })
-                            .setPositiveButton("NEW GAME", new DialogInterface.OnClickListener() {
-                                public void onClick(DialogInterface dialog, int which) {
-                                    resetBoard();
-                                    crossView.updateBoard(board);
-
-
-                                }
-                            })
-
-                            .setIcon(android.R.drawable.ic_dialog_alert)
-                            .show();
-                }
-
 
 
             }
@@ -130,25 +217,25 @@ public class MainActivity extends AppCompatActivity {
 
     private ChekWinner gameOver(int[][] board) {
         for (int i = 0; i < board.length; i++) {
-            ChekWinner chekWinner = chekColWinner(board,i);
+            ChekWinner chekWinner = chekColWinner(board, i);
             if (chekWinner != null) {
                 return chekWinner;
 
             }
         }
         for (int i = 0; i < board.length; i++) {
-            ChekWinner chekWinner = chekRowWinner(board,i);
+            ChekWinner chekWinner = chekRowWinner(board, i);
             if (chekWinner != null) {
                 return chekWinner;
 
             }
         }
 
-        if (board[1][0] == board[1][1] && board[1][0] == board[1][2] && board[1][0] == 1) {
+        if (board[0][0] == board[1][1] && board[0][0] == board[2][2] && board[0][0] == 1) {
             return ChekWinner.X_WIN;
 
         }
-        if (board[2][0] == board[2][1] && board[2][0] == board[2][2] && board[2][0] == 1) {
+        if (board[2][0] == board[1][1] && board[2][0] == board[0][2] && board[2][0] == 1) {
             return ChekWinner.X_WIN;
 
         }
@@ -169,18 +256,17 @@ public class MainActivity extends AppCompatActivity {
         return null;
 
 
-
-
     }
 
     private ChekWinner chekRowWinner(int[][] board, int rowIndex) {
         if (board[0][rowIndex] == board[1][rowIndex] && board[0][rowIndex] == board[2][rowIndex] && board[0][rowIndex] == 1) {
             return ChekWinner.X_WIN;
-        } else if(board[0][rowIndex] == board[1][rowIndex] && board[0][rowIndex] == board[2][rowIndex] && board[0][rowIndex] == -1){
+        } else if (board[0][rowIndex] == board[1][rowIndex] && board[0][rowIndex] == board[2][rowIndex] && board[0][rowIndex] == -1) {
             return ChekWinner.O_WIN;
         }
         return null;
     }
+
     private ChekWinner chekColWinner(int[][] board, int colIndex) {
         if (board[colIndex][0] == board[colIndex][1] && board[colIndex][0] == board[colIndex][2] && board[colIndex][0] == 1) {
             return ChekWinner.X_WIN;
@@ -211,5 +297,28 @@ public class MainActivity extends AppCompatActivity {
                 board[i][j] = 0;
             }
         }
+    }
+
+    public int[] move() {
+        for (int[] move : preferredMoves) {
+            if (content(move[0],move[1]) == Seed.EMPTY) {
+                return move;
+            }
+        }
+        return null;
+    }
+
+    private Seed content(int rowndex, int colIndex ) {
+        if (board[rowndex][colIndex] == 1) {
+            return Seed.CROSS;
+        }else if (board[rowndex][colIndex] == -1) {
+            return Seed.NOUGHT;
+        }
+
+        return Seed.EMPTY;
+    }
+
+    enum Seed {
+        CROSS, EMPTY, NOUGHT
     }
 }
